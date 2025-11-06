deafb71aaa85e37c8d9cd445b1a76f2d42e2feb9
Fettuchoony <wery231@gmail.com>
2025-11-06 15:18:03 -0800

    Initialization

---
+++ csg_cylinder_3d.gd
@@ -0,0 +1,18 @@
+extends CSGCylinder3D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if Input.is_key_pressed(KEY_FORWARD):
+		translate(Vector3.FORWARD)
+	if Input.is_key_pressed(KEY_LEFT):
+		translate(Vector3.LEFT)
+	if Input.is_key_pressed(KEY_RIGHT):
+		translate(Vector3.RIGHT)
+	if Input.is_key_pressed(KEY_BACK):
+		translate(Vector3.BACK)
